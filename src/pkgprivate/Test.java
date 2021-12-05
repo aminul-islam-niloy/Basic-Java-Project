3179461a09de64e047d071c91d9b3e08d047d3da
Aminul Islam Niloy <niloyftaminul@gmail.com>
2021-12-05 23:10:58 +0600

    added file

---
+++ src/pkgprivate/Test.java
@@ -0,0 +1,20 @@
+
+package pkgprivate;
+
+public class Test {
+    public static void main(String[] args) {
+    Teacher t1 = new Teacher();
+    
+    t1.setName("Munna");
+    t1.setAge(26);
+    t1.setQualification("M.Sc in CSE");
+    
+    t1.displayinformation();
+    t1.number=35779435;
+    t1.accountNo=3796954;
+    t1.print();
+    
+
+    }
+    
+}
